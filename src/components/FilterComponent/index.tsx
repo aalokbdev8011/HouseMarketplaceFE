9a28056c662b326d316a591d071ac9113c34eb4d
aloke <alokdev80@gmail.com>
2023-11-07 21:10:10 +0530

    Fixed filter issue

--- src/components/FilterComponent/index.tsx
+++ src/components/FilterComponent/index.tsx
@@ -39,9 +39,9 @@ function FilterComponent({ onSearch }: FilterComponentProps) {
                 <label>Type</label>
                 <select className="appearance-none w-25 bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:border-gray-500 focus:ring focus:ring-gray-200 focus:ring-opacity-50 mr-2" value={propertyType} onChange={(e) => setPropertyType(e.target.value)}>
                     <option value="">Select Type</option>
-                    <option value="Residential">Residential</option>
-                    <option value="Office">Office</option>
-                    <option value="Retail">Retail</option>
+                    <option value="residential">Residential</option>
+                    <option value="office">Office</option>
+                    <option value="retail">Retail</option>
                 </select>
             </div>
             <div className='flex flex-col sm:w-auto w-full'>
